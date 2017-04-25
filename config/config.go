37211f154ab8bac3f0754d8da13ae3753c9e3719
Andrew Nichols <danweasel@gmail.com>
2017-04-25 14:50:42 +0700

    Implemented Lsbeat, added config flag to scan only newly modified files

--- config/config.go
+++ config/config.go
@@ -6,9 +6,13 @@ package config
 import "time"
 
 type Config struct {
-	Period time.Duration `config:"period"`
+	Period  time.Duration `config:"period"`
+	Path    string        `config:"path"`
+	ScanAll bool          `config:"scan_all"`
 }
 
 var DefaultConfig = Config{
-	Period: 1 * time.Second,
+	Period:  1 * time.Second,
+	Path:    ".",
+	ScanAll: true,
 }
